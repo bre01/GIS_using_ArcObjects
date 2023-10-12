bb15dbf75bd8d51e3d0f8ecbd235ed6a432ada7f
bre <iwanttosignup321@gmail.com>
2023-10-12 23:22:04 +0800

    init

---
+++ ex2/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿
+namespace ex2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axMapControl1 = new ESRI.ArcGIS.Controls.AxMapControl();
+            this.axLicenseControl1 = new ESRI.ArcGIS.Controls.AxLicenseControl();
+            this.axLicenseControl2 = new ESRI.ArcGIS.Controls.AxLicenseControl();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.cmdLoadshpf = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.axTOCControl1 = new ESRI.ArcGIS.Controls.AxTOCControl();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.axMapControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axLicenseControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axLicenseControl2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axTOCControl1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axMapControl1
+            // 
+            this.axMapControl1.Location = new System.Drawing.Point(239, 12);
+            this.axMapControl1.Margin = new System.Windows.Forms.Padding(2);
+            this.axMapControl1.Name = "axMapControl1";
+            this.axMapControl1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axMapControl1.OcxState")));
+            this.axMapControl1.Size = new System.Drawing.Size(778, 662);
+            this.axMapControl1.TabIndex = 0;
+            // 
+            // axLicenseControl1
+            // 
+            this.axLicenseControl1.Enabled = true;
+            this.axLicenseControl1.Location = new System.Drawing.Point(337, 189);
+            this.axLicenseControl1.Margin = new System.Windows.Forms.Padding(2);
+            this.axLicenseControl1.Name = "axLicenseControl1";
+            this.axLicenseControl1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axLicenseControl1.OcxState")));
+            this.axLicenseControl1.Size = new System.Drawing.Size(32, 32);
+            this.axLicenseControl1.TabIndex = 1;
+            // 
+            // axLicenseControl2
+            // 
+            this.axLicenseControl2.Enabled = true;
+            this.axLicenseControl2.Location = new System.Drawing.Point(1250, -1);
+            this.axLicenseControl2.Margin = new System.Windows.Forms.Padding(2);
+            this.axLicenseControl2.Name = "axLicenseControl2";
+            this.axLicenseControl2.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axLicenseControl2.OcxState")));
+            this.axLicenseControl2.Size = new System.Drawing.Size(32, 32);
+            this.axLicenseControl2.TabIndex = 2;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // cmdLoadshpf
+            // 
+            this.cmdLoadshpf.Location = new System.Drawing.Point(754, 29);
+            this.cmdLoadshpf.Margin = new System.Windows.Forms.Padding(2);
+            this.cmdLoadshpf.Name = "cmdLoadshpf";
+            this.cmdLoadshpf.Size = new System.Drawing.Size(50, 23);
+            this.cmdLoadshpf.TabIndex = 3;
+            this.cmdLoadshpf.Text = "load shpfile";
+            this.cmdLoadshpf.UseVisualStyleBackColor = true;
+            this.cmdLoadshpf.Click += new System.EventHandler(this.cmdLoadshpf_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(760, 68);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(50, 25);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // axTOCControl1
+            // 
+            this.axTOCControl1.Location = new System.Drawing.Point(13, 33);
+            this.axTOCControl1.Margin = new System.Windows.Forms.Padding(2);
+            this.axTOCControl1.Name = "axTOCControl1";
+            this.axTOCControl1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axTOCControl1.OcxState")));
+            this.axTOCControl1.Size = new System.Drawing.Size(163, 397);
+            this.axTOCControl1.TabIndex = 5;
+            this.axTOCControl1.OnMouseDown += new ESRI.ArcGIS.Controls.ITOCControlEvents_Ax_OnMouseDownEventHandler(this.axTOCControl1_OnMouseDown);
+            this.axTOCControl1.OnEndLabelEdit += new ESRI.ArcGIS.Controls.ITOCControlEvents_Ax_OnEndLabelEditEventHandler(this.axTOCControl1_OnEndLabelEdit);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(17, 299);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(50, 21);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "mdx";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(83, 299);
+            this.button3.Margin = new System.Windows.Forms.Padding(2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(50, 21);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Save";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(854, 456);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.axTOCControl1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.cmdLoadshpf);
+            this.Controls.Add(this.axLicenseControl2);
+            this.Controls.Add(this.axLicenseControl1);
+            this.Controls.Add(this.axMapControl1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.axMapControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axLicenseControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axLicenseControl2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axTOCControl1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ESRI.ArcGIS.Controls.AxMapControl axMapControl1;
+        private ESRI.ArcGIS.Controls.AxLicenseControl axLicenseControl1;
+        private ESRI.ArcGIS.Controls.AxLicenseControl axLicenseControl2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button cmdLoadshpf;
+        private System.Windows.Forms.Button button1;
+        private ESRI.ArcGIS.Controls.AxTOCControl axTOCControl1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ ex2/Form1.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ESRI.ArcGIS.Carto;
+using ESRI.ArcGIS.Controls;
+using ESRI.ArcGIS.Geodatabase;
+using ESRI.ArcGIS.SystemUI;
+
+namespace ex2
+{
+    public partial class Form1 : Form
+    {
+        private ITOCControl2 _passTOCControl;
+        private IMapControl3 _passMapControl;
+        private IToolbarMenu m_menuMap;
+        private IToolbarMenu _TocRightClick;
+        public Form1()
+        {
+            ESRI.ArcGIS.RuntimeManager.BindLicense(ESRI.ArcGIS.ProductCode.EngineOrDesktop);
+            InitializeComponent();
+        }
+
+        private void cmdLoadshpf_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Filter = "shp files(*.shp)|*.shp";
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                Console.WriteLine("ok");
+                Console.WriteLine(openFileDialog1);
+                string fileName = openFileDialog1.FileName;
+                System.IO.Path.GetFileName(fileName);
+                if (axMapControl1.LayerCount != 1)
+                {
+
+                    axMapControl1.AddShapeFile(System.IO.Path.GetDirectoryName(fileName), System.IO.Path.GetFileName(fileName));
+
+
+                }
+                else
+                {
+                    //adding feature to a exsiting layer would take siginificant effort
+                    //Maybe i just add a new layer;
+                    var layer = axMapControl1.get_Layer(0);
+                    Console.WriteLine(axMapControl1.LayerCount);
+                    axMapControl1.AddShapeFile(System.IO.Path.GetDirectoryName(fileName), System.IO.Path.GetFileName(fileName));
+                    Console.WriteLine(axMapControl1.LayerCount);
+                }
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            axMapControl1.ClearLayers();
+        }
+        public void AddFeaturesToExistingLayer(string shapefilePath)
+        {
+            // Get the feature layer you want to add features to.
+            IFeatureLayer featureLayer = null;
+
+            for (int i = 0; i < axMapControl1.LayerCount; i++)
+            {
+                var layer = axMapControl1.get_Layer(i);
+                if (layer is IFeatureLayer && layer.Name == "YourLayerName")
+                {
+                    featureLayer = (IFeatureLayer)layer;
+                    break;
+                }
+            }
+
+            if (featureLayer == null)
+            {
+                // Handle the case where the layer with the specified name is not found.
+                return;
+            }
+
+            // Open an edit session.
+            IWorkspaceEdit workspaceEdit = ((IDataset)featureLayer.FeatureClass).Workspace as IWorkspaceEdit;
+            workspaceEdit.StartEditing(true);
+            workspaceEdit.StartEditOperation();
+
+            try
+            {
+                // Create a feature cursor for inserting features.
+                IFeatureCursor featureCursor = featureLayer.FeatureClass.Insert(true);
+                IFeatureBuffer featureBuffer = featureLayer.FeatureClass.CreateFeatureBuffer();
+
+                // Iterate through your shapefile and add features to the feature class.
+                // You will need to open and read your shapefile to get the feature geometries and attributes.
+
+                // Example:
+                // IFeature feature = ... // Create a new feature with the appropriate geometry and attributes.
+                // featureBuffer.Shape = feature.Shape; // Set the geometry of the feature buffer.
+                // featureBuffer.set_Value(index, value); // Set attribute values.
+
+                // Insert the feature into the feature class.
+                featureCursor.InsertFeature(featureBuffer);
+                featureCursor.Flush();
+
+                // Commit the edit operation and stop the edit session.
+                workspaceEdit.StopEditOperation();
+                workspaceEdit.StopEditing(true);
+            }
+            catch (Exception ex)
+            {
+                // Handle any exceptions that may occur during the editing process.
+                // Roll back the edit operation if necessary.
+                workspaceEdit.AbortEditOperation();
+                workspaceEdit.StopEditing(false);
+            }
+
+            // Refresh the view to display the added features.
+            axMapControl1.Refresh();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            openFileDialog1.Filter="mdx files(*.mxd)|*.mxd";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+
+
+                IMapDocument mapDocument = new MapDocumentClass();
+                /*if (mapDocument != null)
+                {
+                    mapDocument.Close();
+                }*/
+                mapDocument.Open(openFileDialog1.FileName);
+                axMapControl1.Map = mapDocument.ActiveView as IMap;
+
+
+
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SaveDocumentAs();
+        }
+        private void SaveDocument()
+        {
+            IMxdContents mxd = axMapControl1.Map as IMxdContents;
+            IMapDocument mapDocument = new MapDocumentClass();
+            mapDocument.New("C:/Users/bre/Documents/ForGisDev/a.mxd");
+            mapDocument.ReplaceContents(mxd);
+            mapDocument.Save(true, true);
+            MessageBox.Show("saved !");
+        }
+        private void SaveDocumentAs()
+        {
+            saveFileDialog1.Filter="mdx files(*.mxd)|*.mxd";
+            IMxdContents mxd = axMapControl1.Map as IMxdContents;
+            IMapDocument mapDocument = new MapDocumentClass();
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                mapDocument.New(saveFileDialog1.FileName);
+                mapDocument.ReplaceContents(mxd);
+                mapDocument.Save(true, true);
+                MessageBox.Show("saved !");
+            }
+
+        }
+
+        private void axTOCControl1_OnMouseDown(object sender, ITOCControlEvents_OnMouseDownEvent e)
+        {
+            if (e.button != 2)
+            {
+                return;
+            }
+            esriTOCControlItem item = esriTOCControlItem.esriTOCControlItemNone;
+            IBasicMap map = null;
+            ILayer layer = null;
+            object other = null;
+            object index = null;
+            _passTOCControl.HitTest(e.x, e.y, ref item, ref map, ref layer, ref other, ref index);
+            if (item == esriTOCControlItem.esriTOCControlItemMap)
+            {
+                _passTOCControl.SelectItem(map, null);
+            }
+
+            else
+            {
+                _passTOCControl.SelectItem(layer, null);
+            }
+            _passMapControl.CustomProperty = layer;
+                
+            if (item == esriTOCControlItem.esriTOCControlItemLayer)
+                _TocRightClick.PopupMenu(e.x, e.y, _passTOCControl.hWnd);
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //what's the point of this?
+            _passTOCControl = (ITOCControl2)axTOCControl1.Object;
+            _passMapControl = (IMapControl3)axMapControl1.Object;
+            //  
+            _TocRightClick = new ToolbarMenuClass();
+            _TocRightClick.AddItem(new RemoveLayer(), -1, 0, false, esriCommandStyles.esriCommandStyleTextOnly);
+            _TocRightClick.AddItem(new ZoomToLayer(), -1, 1, true, esriCommandStyles.esriCommandStyleTextOnly);
+            //why make a additional variable? it's only used when passing
+            // I can cast it when passing 
+            _TocRightClick.SetHook(_passMapControl);
+            //or I can pass the axMapControl1 and cast it when I need the more specific controls(TOCControl ,MapControl ...)
+
+        }
+
+        private void axTOCControl1_OnEndLabelEdit(object sender, ITOCControlEvents_OnEndLabelEditEvent e)
+        {
+            if ((string)e.newLabel == "")
+            {
+                e.canEdit = false;
+                MessageBox.Show("can not be empty");
+            }
+        }
+    }
+}
